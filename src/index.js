fd21f76a2966726aa15cfb9d80ad21b38b6c14b9
EVGEN <k.e.l@tut.by>
2022-05-08 18:31:35 +0300

    feat: add switching keyboard layouts between English and russian language

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 /* eslint-disable linebreak-style */
 const BUTTON_CODE = ["Backquote", "Digit1", "Digit2", "Digit3", "Digit4", "Digit5", "Digit6", "Digit7", "Digit8", "Digit9", "Digit0", "Minus", "Equal", "Backspace", "Tab", "KeyQ", "KeyW", "KeyE", "KeyR", "KeyT", "KeyY", "KeyU", "KeyI", "KeyO", "KeyP", "BracketLeft", "BracketRight", "Backslash", "CapsLock", "KeyA", "KeyS", "KeyD", "KeyF", "KeyG", "KeyH", "KeyJ", "KeyK", "KeyL", "Semicolon", "Quote", "Enter", "ShiftLeft", "IntlBackslash", "KeyZ", "KeyX", "KeyC", "KeyV", "KeyB", "KeyN", "KeyM", "Comma", "Period", "Slash", "ArrowUp", "ShiftRight", "ControlLeft", "MetaLeft", "AltLeft", "Space", "AltRight", "ControlRight", "ArrowLeft", "ArrowDown", "ArrowRight"];
 const BUTTON_KEY = ["`", "1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "-", "=", "Backspace", "Tab", "q", "w", "e", "r", "t", "y", "u", "i", "o", "p", "[", "]", "\\", "CapsLock", "a", "s", "d", "f", "g", "h", "j", "k", "l", ";", "'", "Enter", "Shift", "\\", "z", "x", "c", "v", "b", "n", "m", ",", ".", "/", "&uarr;", "Shift", "Ctrl", "Win", "Alt", " ", "Alt", "Ctrl", "&larr;", "&darr;", "&rarr;"];
+const BUTTON_KEY_RU = ["ё", "1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "-", "=", "Backspace", "Tab", "й", "ц", "у", "к", "е", "н", "г", "ш", "щ", "з", "х", "ъ", "\\", "CapsLock", "ф", "ы", "в", "а", "п", "р", "о", "л", "д", "ж", "э", "Enter", "Shift", "\\", "я", "ч", "с", "м", "и", "т", "ь", "б", "ю", ".", "&uarr;", "Shift", "Ctrl", "Win", "Alt", " ", "Alt", "Ctrl", "&larr;", "&darr;", "&rarr;"];
 const SHIFT_BUTTON_KEY = ["~", "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "_", "+", "Backspace", "Tab", "q", "w", "e", "r", "t", "y", "u", "i", "o", "p", "{", "}", "|", "CapsLock", "a", "s", "d", "f", "g", "h", "j", "k", "l", ":", "\"", "Enter", "Shift", "|", "z", "x", "c", "v", "b", "n", "m", "<", ">", "?", "&uarr;", "Shift", "Ctrl", "Win", "Alt", " ", "Alt", "Ctrl", "&larr;", "&darr;", "&rarr;"];
+const SHIFT_BUTTON_KEY_RU = ["Ё", "!", "\"", "№", ";", "%", ":", "?", "*", "(", ")", "_", "+", "Backspace", "Tab", "й", "ц", "у", "к", "е", "н", "г", "ш", "щ", "з", "х", "ъ", "/", "CapsLock", "ф", "ы", "в", "а", "п", "р", "о", "л", "д", "ж", "э", "Enter", "Shift", "/", "я", "ч", "с", "м", "и", "т", "ь", "б", "ю", ",", "&uarr;", "Shift", "Ctrl", "Win", "Alt", " ", "Alt", "Ctrl", "&larr;", "&darr;", "&rarr;"];
 
 class VirtualKeyboard {
   constructor() {
@@ -38,14 +40,43 @@ class VirtualKeyboard {
     const CAPS_BUTTON_KEY = BUTTON_KEY.map(
       (el) => ((el.length === 1) ? el.toUpperCase() : el),
     );
+    const CAPS_BUTTON_KEY_RU = BUTTON_KEY_RU.map(
+      (el) => ((el.length === 1) ? el.toUpperCase() : el),
+    );
 
     // ----------------add event to button------------------------
     document.onkeydown = (event) => {
-      // if (this.textArea.blur()) {
-      //   this.textArea.focus();
-      // }
+      if (this.textArea.blur()) {
+        this.textArea.focus();
+      }
       if (event.code === "Backspace") {
         this.textArea.innerHTML = this.textArea.innerHTML.slice(0, -1);
+      } else if (event.altKey && event.shiftKey && this.lang === "en") {
+        this.button = "";
+        if (this.caps === false) {
+          for (let i = 0; i < BUTTON_KEY.length; i += 1) {
+            this.button += `<div class="button button__${this.elementClass[i]}" data-code="${BUTTON_CODE[i]}">${BUTTON_KEY_RU[i]}</div>`;
+          }
+        } else if (this.caps === true) {
+          for (let i = 0; i < BUTTON_KEY.length; i += 1) {
+            this.button += `<div class="button button__${this.elementClass[i]}" data-code="${BUTTON_CODE[i]}">${CAPS_BUTTON_KEY_RU[i]}</div>`;
+          }
+        }
+        this.keyboard.innerHTML = this.button;
+        this.lang = "ru";
+      } else if (event.altKey && event.shiftKey && this.lang === "ru") {
+        this.button = "";
+        if (this.caps === false) {
+          for (let i = 0; i < BUTTON_KEY.length; i += 1) {
+            this.button += `<div class="button button__${this.elementClass[i]}" data-code="${BUTTON_CODE[i]}">${BUTTON_KEY[i]}</div>`;
+          }
+        } else if (this.caps === true) {
+          for (let i = 0; i < BUTTON_KEY.length; i += 1) {
+            this.button += `<div class="button button__${this.elementClass[i]}" data-code="${BUTTON_CODE[i]}">${CAPS_BUTTON_KEY[i]}</div>`;
+          }
+        }
+        this.keyboard.innerHTML = this.button;
+        this.lang = "en";
       } else if (event.code === "Enter") {
         this.textArea.innerHTML += "\n";
       } else if (event.code === "Tab") {
@@ -63,30 +94,54 @@ class VirtualKeyboard {
       } else if (event.code === "CapsLock" && this.caps === false) {
         this.button = "";
         for (let i = 0; i < BUTTON_KEY.length; i += 1) {
-          this.button += `<div class="button button__${this.elementClass[i]}" data-code="${BUTTON_CODE[i]}">${CAPS_BUTTON_KEY[i]}</div>`;
+          if (this.lang === "en") {
+            this.button += `<div class="button button__${this.elementClass[i]}" data-code="${BUTTON_CODE[i]}">${CAPS_BUTTON_KEY[i]}</div>`;
+          } else if (this.lang === "ru") {
+            this.button += `<div class="button button__${this.elementClass[i]}" data-code="${BUTTON_CODE[i]}">${CAPS_BUTTON_KEY_RU[i]}</div>`;
+          }
         }
         this.keyboard.innerHTML = this.button;
         this.caps = true;
       } else if (event.code === "CapsLock" && this.caps === true) {
         this.button = "";
         for (let i = 0; i < BUTTON_KEY.length; i += 1) {
-          this.button += `<div class="button button__${this.elementClass[i]}" data-code="${BUTTON_CODE[i]}">${BUTTON_KEY[i]}</div>`;
+          if (this.lang === "en") {
+            this.button += `<div class="button button__${this.elementClass[i]}" data-code="${BUTTON_CODE[i]}">${BUTTON_KEY[i]}</div>`;
+          } else if (this.lang === "ru") {
+            this.button += `<div class="button button__${this.elementClass[i]}" data-code="${BUTTON_CODE[i]}">${BUTTON_KEY_RU[i]}</div>`;
+          }
         }
         this.keyboard.innerHTML = this.button;
         this.caps = false;
       } else if (event.code.includes("Shift") && this.caps === true) {
         this.button = "";
-        for (let i = 0; i < BUTTON_KEY.length; i += 1) {
-          this.button += `<div class="button button__${this.elementClass[i]}" data-code="${BUTTON_CODE[i]}">${SHIFT_BUTTON_KEY[i]}</div>`;
+        if (this.lang === "en") {
+          for (let i = 0; i < BUTTON_KEY.length; i += 1) {
+            this.button += `<div class="button button__${this.elementClass[i]}" data-code="${BUTTON_CODE[i]}">${SHIFT_BUTTON_KEY[i]}</div>`;
+          }
+        } else if (this.lang === "ru") {
+          for (let i = 0; i < BUTTON_KEY.length; i += 1) {
+            this.button += `<div class="button button__${this.elementClass[i]}" data-code="${BUTTON_CODE[i]}">${SHIFT_BUTTON_KEY_RU[i]}</div>`;
+          }
         }
         this.keyboard.innerHTML = this.button;
       } else if (event.code.includes("Shift") && this.caps === false) {
         this.button = "";
-        for (let i = 0; i < BUTTON_KEY.length; i += 1) {
-          if (SHIFT_BUTTON_KEY[i].length === 1) {
-            this.button += `<div class="button button__${this.elementClass[i]}" data-code="${BUTTON_CODE[i]}">${SHIFT_BUTTON_KEY[i].toUpperCase()}</div>`;
-          } else if (SHIFT_BUTTON_KEY[i].length > 1) {
-            this.button += `<div class="button button__${this.elementClass[i]}" data-code="${BUTTON_CODE[i]}">${SHIFT_BUTTON_KEY[i]}</div>`;
+        if (this.lang === "en") {
+          for (let i = 0; i < BUTTON_KEY.length; i += 1) {
+            if (SHIFT_BUTTON_KEY[i].length === 1) {
+              this.button += `<div class="button button__${this.elementClass[i]}" data-code="${BUTTON_CODE[i]}">${SHIFT_BUTTON_KEY[i].toUpperCase()}</div>`;
+            } else if (SHIFT_BUTTON_KEY[i].length > 1) {
+              this.button += `<div class="button button__${this.elementClass[i]}" data-code="${BUTTON_CODE[i]}">${SHIFT_BUTTON_KEY[i]}</div>`;
+            }
+          }
+        } else if (this.lang === "ru") {
+          for (let i = 0; i < BUTTON_KEY.length; i += 1) {
+            if (SHIFT_BUTTON_KEY[i].length === 1) {
+              this.button += `<div class="button button__${this.elementClass[i]}" data-code="${BUTTON_CODE[i]}">${SHIFT_BUTTON_KEY_RU[i].toUpperCase()}</div>`;
+            } else if (SHIFT_BUTTON_KEY[i].length > 1) {
+              this.button += `<div class="button button__${this.elementClass[i]}" data-code="${BUTTON_CODE[i]}">${SHIFT_BUTTON_KEY_RU[i]}</div>`;
+            }
           }
         }
         this.keyboard.innerHTML = this.button;
@@ -98,19 +153,33 @@ class VirtualKeyboard {
     // ----------------remove active class to button------------------------
     document.onkeyup = (event) => {
       document.querySelector(`.button[data-code="${event.code}"]`).classList.remove("active");
+      // if (!event.altKey && event.shiftKey) {
       if (event.code.includes("Shift") && this.caps === true) {
         this.button = "";
-        for (let i = 0; i < BUTTON_KEY.length; i += 1) {
-          this.button += `<div class="button button__${this.elementClass[i]}" data-code="${BUTTON_CODE[i]}">${CAPS_BUTTON_KEY[i]}</div>`;
+        if (this.lang === "en") {
+          for (let i = 0; i < BUTTON_KEY.length; i += 1) {
+            this.button += `<div class="button button__${this.elementClass[i]}" data-code="${BUTTON_CODE[i]}">${CAPS_BUTTON_KEY[i]}</div>`;
+          }
+        } else if (this.lang === "ru") {
+          for (let i = 0; i < BUTTON_KEY.length; i += 1) {
+            this.button += `<div class="button button__${this.elementClass[i]}" data-code="${BUTTON_CODE[i]}">${CAPS_BUTTON_KEY_RU[i]}</div>`;
+          }
         }
         this.keyboard.innerHTML = this.button;
       } else if (event.code.includes("Shift") && this.caps === false) {
         this.button = "";
-        for (let i = 0; i < BUTTON_KEY.length; i += 1) {
-          this.button += `<div class="button button__${this.elementClass[i]}" data-code="${BUTTON_CODE[i]}">${BUTTON_KEY[i]}</div>`;
+        if (this.lang === "en") {
+          for (let i = 0; i < BUTTON_KEY.length; i += 1) {
+            this.button += `<div class="button button__${this.elementClass[i]}" data-code="${BUTTON_CODE[i]}">${BUTTON_KEY[i]}</div>`;
+          }
+        } else if (this.lang === "ru") {
+          for (let i = 0; i < BUTTON_KEY.length; i += 1) {
+            this.button += `<div class="button button__${this.elementClass[i]}" data-code="${BUTTON_CODE[i]}">${BUTTON_KEY_RU[i]}</div>`;
+          }
         }
         this.keyboard.innerHTML = this.button;
       }
+      // }
     };
 
     // ----------------add event to click------------------------
@@ -127,25 +196,35 @@ class VirtualKeyboard {
           this.textArea.innerHTML += "\t";
         } else if (event.target.innerHTML === "CapsLock" && this.caps === false) {
           for (let i = 0; i < CHAR_BUTTONS.length; i += 1) {
-            if (CHAR_BUTTONS[i].dataset.code.includes("Key")) {
+            if (CHAR_BUTTONS[i].innerHTML.length === 1) {
               CHAR_BUTTONS[i].innerHTML = CHAR_BUTTONS[i].innerHTML.toUpperCase();
             }
           }
           this.caps = true;
         } else if (event.target.innerHTML === "CapsLock" && this.caps === true) {
           for (let i = 0; i < CHAR_BUTTONS.length; i += 1) {
-            if (CHAR_BUTTONS[i].dataset.code.includes("Key")) {
+            if (CHAR_BUTTONS[i].innerHTML.length === 1) {
               CHAR_BUTTONS[i].innerHTML = CHAR_BUTTONS[i].innerHTML.toLowerCase();
             }
           }
           this.caps = false;
         } else if (event.target.innerHTML === "Shift") {
           this.button = "";
-          for (let i = 0; i < BUTTON_KEY.length; i += 1) {
-            if (SHIFT_BUTTON_KEY[i].length === 1) {
-              this.button += `<div class="button button__${this.elementClass[i]}" data-code="${BUTTON_CODE[i]}">${SHIFT_BUTTON_KEY[i].toUpperCase()}</div>`;
-            } else if (SHIFT_BUTTON_KEY[i].length > 1) {
-              this.button += `<div class="button button__${this.elementClass[i]}" data-code="${BUTTON_CODE[i]}">${SHIFT_BUTTON_KEY[i]}</div>`;
+          if (this.lang === "en") {
+            for (let i = 0; i < BUTTON_KEY.length; i += 1) {
+              if (SHIFT_BUTTON_KEY[i].length === 1) {
+                this.button += `<div class="button button__${this.elementClass[i]}" data-code="${BUTTON_CODE[i]}">${SHIFT_BUTTON_KEY[i].toUpperCase()}</div>`;
+              } else if (SHIFT_BUTTON_KEY[i].length > 1) {
+                this.button += `<div class="button button__${this.elementClass[i]}" data-code="${BUTTON_CODE[i]}">${SHIFT_BUTTON_KEY[i]}</div>`;
+              }
+            }
+          } else if (this.lang === "ru") {
+            for (let i = 0; i < BUTTON_KEY.length; i += 1) {
+              if (SHIFT_BUTTON_KEY[i].length === 1) {
+                this.button += `<div class="button button__${this.elementClass[i]}" data-code="${BUTTON_CODE[i]}">${SHIFT_BUTTON_KEY_RU[i].toUpperCase()}</div>`;
+              } else if (SHIFT_BUTTON_KEY[i].length > 1) {
+                this.button += `<div class="button button__${this.elementClass[i]}" data-code="${BUTTON_CODE[i]}">${SHIFT_BUTTON_KEY_RU[i]}</div>`;
+              }
             }
           }
           this.keyboard.innerHTML = this.button;
@@ -160,9 +239,16 @@ class VirtualKeyboard {
       }
       if (event.target.innerHTML === "Shift") {
         this.button = "";
-        for (let i = 0; i < BUTTON_KEY.length; i += 1) {
-          this.button += `<div class="button button__${this.elementClass[i]}" data-code="${BUTTON_CODE[i]}">${BUTTON_KEY[i]}</div>`;
+        if (this.lang === "en") {
+          for (let i = 0; i < BUTTON_KEY.length; i += 1) {
+            this.button += `<div class="button button__${this.elementClass[i]}" data-code="${BUTTON_CODE[i]}">${BUTTON_KEY[i]}</div>`;
+          }
+        } else if (this.lang === "ru") {
+          for (let i = 0; i < BUTTON_KEY.length; i += 1) {
+            this.button += `<div class="button button__${this.elementClass[i]}" data-code="${BUTTON_CODE[i]}">${BUTTON_KEY_RU[i]}</div>`;
+          }
         }
+
         this.keyboard.innerHTML = this.button;
       }
     });
